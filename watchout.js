70311e58d86f5c74b5006fccf63d4045b482c628
Andrew Teich <hackreactor@a673.local>
2014-09-19 12:28:23 -0700

    N Enemies appear at random locations on game board

--- watchout.js
+++ watchout.js
@@ -1,2 +1,21 @@
 // start slingin' some d3 here.
+var body = d3.select("body");
+var board = d3.select("#board");
+console.log(board);
+var enemyLocations = [];
+var numEnemies = 10;
 
+//Game board
+var boardWidth = 700;
+var boardHeight = 470;
+
+for(var i=0; i<numEnemies; i++){
+	enemyLocations.push([Math.floor(Math.random()*boardWidth), Math.floor(Math.random()*boardHeight)])
+}
+
+var allEnemies = board.selectAll("svg").data(enemyLocations).enter().append('circle');
+allEnemies.attr('class', 'enemy').attr('cx', function(d){
+	return d[0];	
+}).attr('cy', function(d){
+	return d[1];
+}).attr('r', 10);
